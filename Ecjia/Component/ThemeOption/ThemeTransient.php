b7bd3635119f7458782d5ad664cfeeed1d3a4413
王正东 <wangzhengdong@ecmoban.com>
2020-04-01 18:06:58 +0800

    add ecjia/theme-option组件

---
+++ Ecjia/Component/ThemeOption/ThemeTransient.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: royalwang
+ * Date: 2018/11/23
+ * Time: 09:43
+ */
+
+namespace Ecjia\Component\ThemeOption;
+
+use Ecjia\System\Frameworks\Component\Transient;
+
+class ThemeTransient extends Transient
+{
+
+
+}
