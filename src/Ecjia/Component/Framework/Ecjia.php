d108ad83a107b1e70f847005c106c6cc20e877ef
王正东 <wangzhengdong@ecmoban.com>
2020-09-18 14:53:34 +0800

    更新ecjia系统版本为v2.6.0

--- src/Ecjia/Component/Framework/Ecjia.php
+++ src/Ecjia/Component/Framework/Ecjia.php
@@ -61,9 +61,9 @@ class Ecjia extends Container
 {
     use Macroable;
 
-    const VERSION = '2.5.0';
+    const VERSION = '2.6.0';
 
-    const RELEASE = '20200813';
+    const RELEASE = '20200918';
 
     protected $config;
 
